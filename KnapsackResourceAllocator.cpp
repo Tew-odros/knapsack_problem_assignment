0d1ad045c12725af29f203f60f802ed45da510f4
Tewodros Besha <tewodrosbesha@gmail.com>
2025-06-07 23:46:40 +0300

    import important header files and declare variables

---
+++ KnapsackResourceAllocator.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <iomanip>
+#include <algorithm>
+#include <limits>
+using namespace std;
+
+struct Item {
+    int value;
+    int weight;
+    int index;
+};
+
+struct FractionalItem {
+    int index;
+    double fraction;
+};
+
+int main () {
+
+    return 0;
+}
